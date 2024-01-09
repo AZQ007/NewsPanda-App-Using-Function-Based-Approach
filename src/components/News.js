4e09b3b85d2d2d9abc2b18c47305676ae61eb535
AZQ007 <ahmadzaheer7991@gmail.com>
2024-01-09 20:10:39 +0500

    Second Project(NewPanda) Completed using Component based Structure

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -22,22 +23,26 @@ export class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults: 0
     };
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsPanda`;
   }
 
   async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d138aec2e5a742f8b8c2441237303545&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+    this.props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
@@ -53,14 +58,31 @@ export class News extends Component {
     this.updateNews();
   };
 
+  fetchMoreData = async() => {
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    })
+  };
+
   render() {
     return (
-      <div className="container my-3">
+        <>
         <h1 className="text-center my-5">NewsPanda - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+        <div className="container">
         <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
+          {this.state.articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
@@ -76,29 +98,9 @@ export class News extends Component {
               );
             })}
         </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handlePreviousClick}
-          >
-            {" "}
-            &larr; Previous
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
-            Next &rarr;{" "}
-          </button>
         </div>
-      </div>
+        </InfiniteScroll>
+      </>
     );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,8 +6,9 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
             <div className="card">
-            <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
-                      {source}</span>
+              <div style={{display:'flex', justifyContent:'flex-end',position:'absolute',right:'0'}}>
+                <span className="badge rounded-pill bg-danger">{source}</span>
+              </div>
                 <img src={!imageUrl?"https://s.yimg.com/ny/api/res/1.2/HAHBspnDaSTe2jgpno.GBw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://media.zenfs.com/en/bloomberg_markets_842/c078deb2a04986337a8a7c215bed1e3b":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
